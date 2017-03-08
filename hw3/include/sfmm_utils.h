7e0810da8b693186196d4aefca127c1b27c5641c
Kuba Gasiorowski <kgasiorowski@cs.stonybrook.edu>
2017-03-08 18:11:57 -0500

    Further implemented underlying utility functions, next implement coalescing of free blocks

--- hw3/include/sfmm_utils.h
+++ hw3/include/sfmm_utils.h
@@ -8,5 +8,8 @@ void dummy(void*);
 int roundup(double);
 void *allocate_from_free_block(sf_free_header*, size_t);
 void insert_into_freelist(sf_free_header*);
+sf_free_header* find_match(size_t);
+sf_free_header *coalesce(sf_free_header*);
+int freelist_length();
 
 #endif
--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -17,6 +17,7 @@
  */
 sf_free_header* freelist_head = NULL;
 
+//TODO - IMPLEMENT ME
 void *sf_malloc(size_t size) {
 
 	if(size > 4*PAGE_SIZE){
@@ -26,10 +27,11 @@ void *sf_malloc(size_t size) {
 		return NULL;
 
 	//Calculate the total block size of what the user is requesting.
-	size_t totalBlockSize = (roundup(size/16.0)*16)+SF_FOOTER_SIZE+SF_HEADER_SIZE;
+	size_t totalBlockSize = (roundup(size/LINE_SIZE)*LINE_SIZE)+SF_FOOTER_SIZE+SF_HEADER_SIZE;
+	sf_free_header *block_to_alloc = find_match(size);
 
 	//Freelist is empty, which means theres no free space. We must ask for more.
-	if(freelist_head == NULL){
+	if(block_to_alloc == NULL){
 
 		void *baseBlockLocation = sf_sbrk(totalBlockSize);
 
@@ -52,12 +54,12 @@ void *sf_malloc(size_t size) {
 		new_page_footer->block_size = new_block_size>>4;
 
 		insert_into_freelist(free_page);
-
-		return allocate_from_free_block(baseBlockLocation, size);
+		debug("Freelist length: %d\n", freelist_length());
+		block_to_alloc = find_match(size);
 
 	}
 
-	return (void*)0;
+	return allocate_from_free_block(block_to_alloc, size);
 
 }
 
@@ -89,43 +91,61 @@ int roundup(double input){
 
 }
 
-//TODO LOOK ME OVER
+//Returns null if no matching block can be found.
 sf_free_header* find_match(size_t requested_size){
+
+	debug("Entered find match with size: %d\n", (int)requested_size);
 
 	sf_free_header* cursor = freelist_head;
 	sf_free_header* closest_match = NULL;
-	int smallest_difference = (unsigned int)-1;
+	int smallest_difference = PAGE_SIZE*100;
 
 	int difference = 0;
 	size_t current_block_size = -1;
-	size_t requested_block_size = (roundup(requested_size/16.0)*16)+SF_FOOTER_SIZE+SF_HEADER_SIZE;
+	size_t requested_block_size = (roundup(requested_size/LINE_SIZE)*LINE_SIZE)+SF_FOOTER_SIZE+SF_HEADER_SIZE;
+	debug("Requested block size: %d\n", (int)requested_block_size);
 
-
+	//Iterate through the linked list
 	while(cursor != NULL){
 
+		//Get the current block size
 		current_block_size = cursor->header.block_size<<4;
+		debug("Current block size: %d\n", (int)current_block_size);
+
+		//Find the difference between the current block size, and the
+		//needed block size
 		difference = current_block_size-requested_block_size;
+		debug("Difference calculated: %d\n", (int)difference);
 
 		//Move on if the block is too small
 		if(difference < 0)
 			continue;
 
+		//If a perfect match was found,
+		//Immediately return it
 		if(difference == 0){
-			//Perfect match found
-			//Immediately return it
-			return cursor;
+			closest_match = cursor;
+			return closest_match;
 		}
 
-		if(difference < smallest_difference)
+		//If this difference was the smallest so far,
+		//Set it as the smallest difference.
+		if(difference < smallest_difference){
+			smallest_difference = difference;
 			closest_match = cursor;
+		}
 
+		//Iterate
 		cursor = cursor->next;
 	}
 
+	debug("Returning closest_match: (%p)\n", closest_match);
 	return closest_match;
 
 }
 
+//Removes a free block from the free list
+//Essentially a deletion from a linked list
 void remove_from_freelist(sf_free_header* block_to_remove){
 
 	sf_free_header *cursor = freelist_head;
@@ -177,9 +197,11 @@ void remove_from_freelist(sf_free_header* block_to_remove){
 	cursor->next->prev = cursor->prev;
 	cursor->next = NULL;
 	cursor->prev = NULL;
+	return;
 
 }
 
+//Inserts a free block into the free list, sorted by address ascending
 void insert_into_freelist(sf_free_header* block_to_insert){
 
 	debug("Entered insertion function with (%p) with block size (%d)\n", (void*)block_to_insert, block_to_insert->header.block_size<<4);
@@ -224,6 +246,14 @@ void insert_into_freelist(sf_free_header* block_to_insert){
 
 }
 
+//Coalesces a free block with any surrounding free blocks
+//TODO
+sf_free_header *coalesce(sf_free_header* block_to_coalesce){
+
+	return NULL;
+
+}
+
 //Takes a free block, and cuts it into two pieces:
 //One allocated block which is returned, and one free
 //block which is re-inserted into the freeblock linked list
@@ -248,7 +278,7 @@ void *allocate_from_free_block(sf_free_header* freeblock, size_t requested_size)
 	size_t padding_size;
 	size_t block_size;
 
-	payload_size = roundup(requested_size/16.0)*16;
+	payload_size = roundup(requested_size/LINE_SIZE)*LINE_SIZE;
 	padding_size = payload_size - requested_size;
 	block_size = payload_size + SF_FOOTER_SIZE + SF_HEADER_SIZE;
 
@@ -274,22 +304,46 @@ void *allocate_from_free_block(sf_free_header* freeblock, size_t requested_size)
 
 	//Create a new free header for the block we are carving up
 	sf_free_header *newFreeBlock = (void*)((alloc_footer)+1);
+	//Calculate the new free block size
+	size_t new_free_block_size = (original_free_block_size - block_size)>>4;
+
 	debug("New free header address: %p\n", (void*)newFreeBlock);
 	newFreeBlock->header.alloc = 0;
 	newFreeBlock->header.splinter = 0;
-	newFreeBlock->header.block_size = (original_free_block_size - block_size)>>4;
+	newFreeBlock->header.block_size = new_free_block_size;
 	newFreeBlock->header.requested_size = 0;
 	newFreeBlock->header.splinter_size = 0;
 	newFreeBlock->header.padding_size = 0;
 
 	//Update the fields in the old free block footer
-	original_free_footer->block_size = (original_free_block_size - block_size)>>4;
+	original_free_footer->block_size = new_free_block_size;
 	debug("Free block footer address: %p\n", (void*)original_free_footer);
 
 	insert_into_freelist(newFreeBlock);
+	debug("Freelist length: %d\n", freelist_length());
 
 	return (void*)(alloc_header+1);
 
 }
 
+//Returns the length of the free list.
+int freelist_length(){
+
+	int i = 0;
+
+	sf_free_header *cursor = freelist_head;
+
+	while(cursor != NULL){
+
+		i++;
+		cursor = cursor->next;
+
+	}
+
+	return i;
+
+}
+
+//Serves no purpose other than to get rid of
+//Unused variable warnings
 void dummy(void* dummy){}
